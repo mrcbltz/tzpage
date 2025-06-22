ea30fb5f1e1c02ad0bbf5490218b6693b3a8640f
mrgrzk <mr.grzk@gmail.com>
2025-06-22 18:24:20 +0200

    0.11

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import Features from "@/components/home/features";
 import FooterSection from "@/components/home/footer";
 import HeroSection from "@/components/home/hero-section";
 import LogoCloud from "@/components/home/logo-cloud";
-import Zdjecie from "@/components/home/zdjecie";
-import Image from "next/image";
+
 
 export default function Home() {
   return (
